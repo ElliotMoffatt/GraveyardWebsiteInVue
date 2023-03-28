bad4f5d2213faaf1abf0c815d503406be7f65632
munkfish <munkf@MUNKFISHTOWER>
2023-03-28 23:32:56 +0100

    reverted the setting that broke everything

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,5 @@
 const webpack = require('webpack')
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'production' ? '/GraveyardWebsiteInVue/' : '/',
     configureWebpack: {
         plugins: [
             new webpack.ProvidePlugin({
